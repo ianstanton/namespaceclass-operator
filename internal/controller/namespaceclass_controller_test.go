5c63b2de87f4e0623279407e654655ede96dad8d
Ian Stanton <ian@stanton.sh>
2025-05-06 08:23:37 -0400

    Add test for create namespace
    Signed-off-by: Ian Stanton <ian@stanton.sh>

--- internal/controller/namespaceclass_controller_test.go
+++ internal/controller/namespaceclass_controller_test.go
@@ -1,11 +1,15 @@
-package controller_test
+package controller
 
 import (
 	akuityiov1 "akuity.io/namespace-class/api/v1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"time"
 )
 
 var _ = Describe("NamespaceclassController", func() {
@@ -29,6 +33,67 @@ var _ = Describe("NamespaceclassController", func() {
 				}
 				Expect(namespaceClass).NotTo(BeNil())
 				Expect(namespaceClass.Name).To(Equal("test-namespaceclass"))
+			})
+		})
+		Context("When creating a Namespace with a valid NamespaceClass label", func() {
+			It("Should create resources defined in the NamespaceClass", func(ctx SpecContext) {
+				nsclass := &akuityiov1.NamespaceClass{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "NamespaceClass",
+						APIVersion: "akuity.io/v1",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "public-network",
+					},
+					Spec: akuityiov1.NamespaceClassSpec{
+						Resources: []runtime.RawExtension{
+							{
+								Object: &networkingv1.NetworkPolicy{
+									TypeMeta: metav1.TypeMeta{
+										Kind:       "NetworkPolicy",
+										APIVersion: "networking.k8s.io/v1",
+									},
+									ObjectMeta: metav1.ObjectMeta{
+										Name: "allow-all-ingress",
+									},
+									Spec: networkingv1.NetworkPolicySpec{
+										PodSelector: metav1.LabelSelector{
+											MatchLabels: map[string]string{
+												"app": "my-app",
+											},
+										},
+										Ingress: []networkingv1.NetworkPolicyIngressRule{},
+									},
+								},
+							},
+						},
+					},
+				}
+				// Create a NamespaceClass
+				Expect(k8sClient.Create(ctx, nsclass)).Should(Succeed())
+				// Create Namespace with the label 'namespaceclass.akuity.io/name: public-network'
+				namespace := &corev1.Namespace{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "Namespace",
+						APIVersion: "v1",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "my-app",
+						Labels: map[string]string{
+							"namespaceclass.akuity.io/name": "public-network",
+						},
+					},
+				}
+				Expect(k8sClient.Create(ctx, namespace)).Should(Succeed())
+				// Check if the NetworkPolicy is created
+				networkPolicy := &networkingv1.NetworkPolicy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "allow-all-ingress",
+					},
+				}
+				// Wait for the controller to reconcile, sleep for now
+				time.Sleep(1 * time.Second)
+				Expect(k8sClient.Get(ctx, types.NamespacedName{Namespace: "my-app", Name: "allow-all-ingress"}, networkPolicy)).Should(Succeed())
 			})
 		})
 	})
