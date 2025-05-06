b379ead60a8bceaeb1bd1e4a1fcfa575ab3104b0
Ian Stanton <ian@stanton.sh>
2025-05-05 23:31:44 -0400

    Deserialize and Create / Update Resources (#7)
    * First pass at deserializing resources
    
    Signed-off-by: Ian Stanton <ian@stanton.sh>
    
    * fmt
    
    Signed-off-by: Ian Stanton <ian@stanton.sh>
    
    * Create or update resources
    
    Signed-off-by: Ian Stanton <ian@stanton.sh>
    
    ---------
    
    Signed-off-by: Ian Stanton <ian@stanton.sh>

--- internal/controller/namespaceclass_controller.go
+++ internal/controller/namespaceclass_controller.go
@@ -19,6 +19,9 @@ package controller
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 
@@ -36,6 +39,13 @@ type NamespaceClassReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+// NewDecoder is used for deserializing RawExtension objects from the
+// NamespaceClassSpec.Resources field.
+func NewDecoder(scheme *runtime.Scheme) runtime.Decoder {
+	codecs := serializer.NewCodecFactory(scheme)
+	return codecs.UniversalDeserializer()
+}
+
 const NamespaceClassLabel = "namespaceclass.akuity.io/name"
 
 // +kubebuilder:rbac:groups=akuity.io,resources=namespaceclasses,verbs=get;list;watch;create;update;patch;delete
@@ -44,13 +54,6 @@ const NamespaceClassLabel = "namespaceclass.akuity.io/name"
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the NamespaceClass object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.4/pkg/reconcile
 func (r *NamespaceClassReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = logf.FromContext(ctx)
 
@@ -73,8 +76,82 @@ func (r *NamespaceClassReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 	} else {
 		// The namespace has the label, so for now we'll log the value
 		logf.FromContext(ctx).Info("Namespace has a namespaceclass label", "labelValue", labelValue)
-	}
 
+		// Check for the NamespaceClass resource with the same name as the label value
+		namespaceClass := &akuityiov1.NamespaceClass{}
+
+		// Log error if the NamespaceClass is not found
+		if err := r.Get(ctx, client.ObjectKey{Name: labelValue}, namespaceClass); err != nil {
+			logf.FromContext(ctx).Error(err, "cannot find NamespaceClass with name", "name", labelValue)
+			return ctrl.Result{}, nil
+		}
+
+		// Log the NamespaceClass we found
+		logf.FromContext(ctx).Info("Found NamespaceClass", "namespaceClass", namespaceClass)
+
+		// Reconcile the resources defined in the NamespaceClass
+		decoder := NewDecoder(r.Scheme)
+		for _, resource := range namespaceClass.Spec.Resources {
+			logf.FromContext(ctx).Info("Reconciling resource", "resource", resource)
+			obj, _, err := decoder.Decode(resource.Raw, nil, nil)
+			if err != nil {
+				// Log error if deserializing fails
+				logf.FromContext(ctx).Error(err, "failed to decode resource", "resource", resource)
+				continue
+			}
+			// Create or update the resource in the namespace
+			// Type assertion to implement client.Object interface
+			typedObj, ok := obj.(client.Object)
+			if !ok {
+				logf.FromContext(ctx).Error(nil, "could not implement client.Object interface via type assertion", "resource", resource)
+				continue
+			}
+
+			// Check if the resource already exists
+			err = r.Get(ctx, client.ObjectKey{Namespace: namespace.Name, Name: typedObj.GetName()}, typedObj)
+			if err == nil {
+				// Resource already exists, so we update it
+				if err := r.Client.Update(ctx, typedObj); err != nil {
+					logf.FromContext(ctx).Error(err, "failed to update resource", "resource", typedObj)
+					return ctrl.Result{}, err
+				}
+				continue
+			}
+
+			_, err = ctrl.CreateOrUpdate(ctx, r.Client, typedObj, func() error {
+				// Set the owner reference to the NamespaceClass
+				if err := ctrl.SetControllerReference(namespaceClass, typedObj, r.Scheme); err != nil {
+					return err
+				}
+				// Set the namespace to the one being reconciled
+				typedObj.SetNamespace(namespace.Name)
+				// Set the labels to include the NamespaceClass label
+				labels := typedObj.GetLabels()
+				if labels == nil {
+					labels = make(map[string]string)
+				}
+				labels[NamespaceClassLabel] = namespaceClass.Name
+				typedObj.SetLabels(labels)
+
+				// Create or update the resource
+				if err := r.Client.Create(ctx, typedObj); err != nil {
+					if meta.IsNoMatchError(err) {
+						logf.FromContext(ctx).Error(err, "resource type not supported", "resource", resource)
+						return nil
+					}
+					logf.FromContext(ctx).Error(err, "failed to create resource", "resource", typedObj)
+					return err
+				}
+				logf.FromContext(ctx).Info("Resource created", "resource", typedObj)
+
+				return nil
+			})
+			if err != nil {
+				logf.FromContext(ctx).Error(err, "failed to reconcile resource", "resource", typedObj)
+				return ctrl.Result{}, err
+			}
+		}
+	}
 	return ctrl.Result{}, nil
 }
 
