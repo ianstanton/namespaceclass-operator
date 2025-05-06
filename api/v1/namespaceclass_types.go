23bf500eb3571d4ce33cf78d3642e6c65370ce9a
Ian Stanton <ian@stanton.sh>
2025-05-05 21:21:16 -0400

    Add resources spec value and update sample
    Signed-off-by: Ian Stanton <ian@stanton.sh>

--- api/v1/namespaceclass_types.go
+++ api/v1/namespaceclass_types.go
@@ -18,18 +18,13 @@ package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // NamespaceClassSpec defines the desired state of NamespaceClass.
 type NamespaceClassSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of NamespaceClass. Edit namespaceclass_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Resources is a list of resources to be created within a namespace
+	Resources []runtime.RawExtension `json:"resources,omitempty"`
 }
 
 // NamespaceClassStatus defines the observed state of NamespaceClass.
